1daefd8bd050e9f5e61f9cffbeadd30bb9a10a79
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-12-30 16:41:48 +0700

    feat(): update to standalone component

--- apps/gitropolis-app/src/app/app.component.ts
+++ apps/gitropolis-app/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'gitropolis-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  template: `
+    <div class="container-lg p-responsive">
+      <h1>Gitropolis</h1>
+    </div>
+    <router-outlet></router-outlet>  `,
+  styles: [],
+  imports: [
+    RouterOutlet
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'gitropolis-app';
